9321fe104df9efefb59aaf8425a27579f95ce426
ealitten <ea.litten@gmail.com>
2018-01-22 15:38:36 +0000

    Update test

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,5 +1,7 @@
 feature 'Homepage' do
   scenario 'User can see list of links on homepage' do
+    Bookmark.create(url: "http://www.makersacademy.com", title: "My test bookmark")
+    visit ('/')
     expect(page).to have_link("My test bookmark")
   end
 end

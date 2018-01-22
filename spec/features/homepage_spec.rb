cec471eedb1bb71e50970decbd210cb5425a0f01
ealitten <ea.litten@gmail.com>
2018-01-22 16:43:25 +0000

    Creating a modular Sinatra app

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,7 @@
-feature 'Homepage' do
+feature 'Bookmark list' do
   scenario 'User can see list of links on homepage' do
     Bookmark.create(url: "http://www.makersacademy.com", title: "My test bookmark")
-    visit ('/')
-    expect(page).to have_link("My test bookmark")
+    visit ('/links')
+    expect(page).to have_content("My test bookmark")
   end
 end

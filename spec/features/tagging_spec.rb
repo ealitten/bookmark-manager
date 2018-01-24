cea3d7fefc7e2e808357706b93cef591cd5845a0
ealitten <ea.litten@gmail.com>
2018-01-24 16:05:53 +0000

    Add test for adding multiple tags when creating BM

--- spec/features/tagging_spec.rb
+++ spec/features/tagging_spec.rb
@@ -8,4 +8,16 @@ feature 'Add tags to bookmarks' do
     bookmark = Bookmark.first
     expect(bookmark.tags.map(&:name)).to include('test-tag')
   end
+
+  scenario 'User adds a new bookmark with several tags' do
+    visit ('/links/new')
+    fill_in 'title', with: 'google'
+    fill_in 'url', with: 'www.google.com'
+    fill_in 'tags', with: 'test-tag, bubbles, ruby'
+    click_button 'Submit'
+    bookmark = Bookmark.first
+    expect(bookmark.tags.map(&:name)).to include('test-tag')
+    expect(bookmark.tags.map(&:name)).to include('bubbles')
+    expect(bookmark.tags.map(&:name)).to include('ruby')
+  end
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,16 +1,14 @@
-feature 'Bookmark list' do
-  scenario 'User can see list of links on homepage' do
+feature 'View bookmark list' do
+  scenario 'User can see list of bookmarks on homepage' do
     Bookmark.create(url: "http://www.makersacademy.com", title: "My test bookmark")
     visit ('/links')
     expect(page).to have_content("My test bookmark")
   end
 
-  scenario 'User can filter links by tags' do
+  scenario 'User can filter bookmarks by tags' do
     Bookmark.create(url: "http://www.makersacademy.com", title: "My test bookmark", tags: [Tag.first_or_create(name: 'ruby')])
     Bookmark.create(url: "http://www.google.com", title: "My bubbling bookmark", tags: [Tag.first_or_create(name: 'bubbles')])
-
     visit ('/tags/bubbles')
-
     expect(page).to have_content("My bubbling bookmark")
     expect(page).not_to have_content("My test bookmark")
   end

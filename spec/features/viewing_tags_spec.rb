0c4b2a9cee4358dd6e8d6af78f8f0b09196b3070
ealitten <ea.litten@gmail.com>
2018-01-24 12:58:08 +0000

    Add feature test for viewing tags

---
+++ spec/features/viewing_tags_spec.rb
@@ -0,0 +1,10 @@
+feature 'View bookmarks with a specific tage' do
+  scenario 'User visits /tags/bubbles and only sees BMs with that tag' do
+    fill_in 'title', with: 'bubbly bookmark'
+    fill_in 'url', with: 'www.google.com'
+    fill_in 'tags', with: 'bubbles'
+    click_button 'Submit'
+    visit ('/tags/bubbles')
+    expect(page).to have_content('bubbles')
+  end
+end
